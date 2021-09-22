3e4a86f0af271b28a95c4fb3a2c868c251ddb6bf
Diego Santizo <88704026+diegosantizo@users.noreply.github.com>
2021-09-22 13:45:41 -0600

    actualizar
    .

---
+++ crud_productos.php
@@ -0,0 +1,29 @@
+<?php
+    if( !empty($_POST) ){
+
+       $txt_id = utf8_decode($_POST["txt_id"]);
+        $txt_producto = utf8_decode($_POST["txt_producto"]);
+        $drop_marca = utf8_decode($_POST["drop_marcas"]);
+        $txt_descripcion = utf8_decode($_POST["txt_descripcion"]);
+        $txt_costo = utf8_decode($_POST["txt_costo"]);
+        $txt_venta = utf8_decode($_POST["txt_venta"]);
+        $txt_existencia = utf8_decode($_POST["txt_existencia"]);
+        
+        include("datos_conexion.php");
+        $db_conexion = mysqli_connect($db_host,$db_usr,$db_pass,$db_nombre);
+        $sql ="";
+
+        if(isset($_POST['btn_agregar'])  ){
+          $sql = "INSERT INTO InvProductos.Productos(producto,idMarca,Descripcion,precio_costo,precio_venta,existencia) VALUES ('". $txt_producto ."','". $drop_marca ."','". $txt_descripcion ."','". $txt_costo ."','". $txt_venta ."','". $txt_existencia .");";
+        }else if( isset($_POST['btn_modificar'])  ){
+          $sql = "UPDATE Productos SET producto='". $txt_producto ."',marca='". $drop_marca ."',Descripcion='". $txt_Descripcion ."',precio_costo='". $txt_costo ."',precio_venta='". $txt_venta ."',existencia='". $txt_existencia ." where producto = ". $txt_producto.";";
+        }else if( isset($_POST['btn_eliminar'])  ){
+          $sql = "DELETE FROM Productos  WHERE producto = ". $txt_producto.";";
+        }else if ($db_conexion->query($sql)===true){
+            $db_conexion->close();
+            header('Location: /parcial2%202021/Parcial2_Productos/index.php');
+        }else{
+            $db_conexion->close();
+        }
+    }
+?>
